28f608e29d888eb0b8dc2fb012291bbfc950e261
Mino Zang <minoscc+dev@gmail.com>
2020-10-03 01:22:00 +0800

    chore: 历史表加上 phone

--- src/shared/utils/db.js
+++ src/shared/utils/db.js
@@ -3,7 +3,7 @@ import Dexie from 'dexie';
 const db = new Dexie('ExpressPlus');
 db.version(1).stores({
 	favorites: 'postId,type,phone,state,pin,*tags,createdAt,updatedAt,message',
-	histories: 'postId,type,updatedAt',
+	histories: 'postId,type,phone,updatedAt',
 	settings: 'key,value',
 });
 
