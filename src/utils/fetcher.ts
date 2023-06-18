c7a0669a388826d569306188895e28ebc355ee55
mino <minoscc+dev@gmail.com>
2023-06-18 15:12:34 +0800

    chore: 请求添加超时 10 秒

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,14 +1,26 @@
 import { runtime } from 'webextension-polyfill';
 import { log } from './log';
 
+const TIMEOUT = 10 * 1000;
+
 export const fetcher = async <T = any>(url: string | [string, ...any], data?: any) => {
   log('req >', url, data);
 
   const kind = Array.isArray(url) ? url[0] : url;
-  const res = await runtime.sendMessage({
-    kind,
-    data,
-  });
-  log('res >', res);
+
+  const res = await Promise.race([
+    runtime.sendMessage({
+      kind,
+      data,
+    }),
+    new Promise((_resolve, reject) => {
+      setTimeout(() => {
+        reject(new Error('请求超时，请稍候重试'));
+      }, TIMEOUT);
+    }),
+  ]);
+
+  log('res <', res);
+
   return res as T;
 };
