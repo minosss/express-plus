2352509517ef3b72a97b0c864fd73f17a3293500
mino <minoscc+dev@gmail.com>
2023-06-22 17:17:25 +0800

    fix: 修复查询记录没有更新

--- src/pages/detail/use-detail.ts
+++ src/pages/detail/use-detail.ts
@@ -1,8 +1,9 @@
 import type { QueryItem } from '../../api/types';
 import type { Track } from '../../types';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { useAtom } from 'jotai';
 import { useEffect } from 'react';
+import { showNotification } from '@mantine/notifications';
 import { MessageKind } from '../../types';
 import { fetcher } from '../../utils/fetcher';
 import { now } from '../../utils/helper';
@@ -10,12 +11,19 @@ import { queryAtom } from './store';
 
 export function useDetail() {
   const [query, setQuery] = useAtom(queryAtom);
+  const queryClient = useQueryClient();
 
-  const { data, isLoading, error, refetch: refetchDetail } = useQuery({
+  const { data, isLoading, error, refetch: refetchDetail, isFetched } = useQuery({
     queryKey: ['query', query],
     queryFn: async () => fetcher<QueryItem>(MessageKind.Query, query),
     enabled: query != null,
   });
+
+  useEffect(() => {
+    if (isFetched) {
+      queryClient.invalidateQueries(['history']);
+    }
+  }, [isFetched, queryClient]);
 
   function cleanQuery() {
     setQuery(null);
@@ -39,7 +47,12 @@ export function useDetail() {
     if (isTracking && query?.id) {
       await fetcher(MessageKind.DeleteTrack, query.id);
       await refetch();
-    } else if (data != null) {
+    } else if (data == null) {
+      showNotification({
+        color: 'red',
+        message: '数据错误，无法收藏',
+      });
+    } else {
       await fetcher(MessageKind.PutTrack, {
         ...query,
         state: data.state,
