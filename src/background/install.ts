ef049bdc2afd2d28547d5ea184ed4cab61be7703
mino <minoscc+dev@gmail.com>
2023-07-15 09:17:47 +0800

    fix: 修复 offscreen 没有初始化导致无法查询

--- src/background/install.ts
+++ src/background/install.ts
@@ -2,10 +2,10 @@ import { runtime } from 'webextension-polyfill';
 import { resetAlarm } from './alarm';
 import { createKuaidiDocument } from './offscreen';
 
-runtime.onInstalled.addListener((details) => {
-  createKuaidiDocument();
-  resetAlarm();
+createKuaidiDocument();
+resetAlarm();
 
+runtime.onInstalled.addListener((details) => {
   if (details.reason === 'install') {
     // runtime.setUninstallURL('')
   }
